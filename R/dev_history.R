0ff43ae8e53b1c6f032450ff117a24a0e057b29d
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-07-17 02:49:20 +0800

    rename pkg into lowercases. related to  https://github.com/JiaxiangBU/learn_dev_py_pkg/issues/1#issuecomment-511939763

--- R/dev_history.R
+++ R/dev_history.R
@@ -1,15 +1,15 @@
 file.copy("../mortgage_forecasts/LICENSE", ".")
-package_name <- "pyKS"
+package_name <- "pyks"
 
 # once --------------------------------------------------------------------
 
 file.copy("../mortgage_forecasts/setup.py", ".")
 file.copy("../mortgage_forecasts/version.py", ".")
 file.copy("../mortgage_forecasts/meta.yaml", ".")
-dir.create("pyKS")
-file.copy("../mortgage_forecasts/mortgage_forecasts/__init__.py", "pyKS/", overwrite = TRUE)
-file.copy("../mortgage_forecasts/mortgage_forecasts/models.py", "pyKS/ks.py", overwrite = TRUE)
-file.edit("pyKS/__init__.py")
+dir.create("pyks")
+file.copy("../mortgage_forecasts/mortgage_forecasts/__init__.py", "pyks/", overwrite = TRUE)
+file.copy("../mortgage_forecasts/mortgage_forecasts/models.py", "pyks/ks.py", overwrite = TRUE)
+file.edit("pyks/__init__.py")
 
 use_readme_rmd()
 file.copy("../mortgage_forecasts/README.Rmd", "README.Rmd", overwrite = TRUE)
@@ -20,5 +20,5 @@ read_file("README.Rmd") %>% str_replace_all("mortgage_forecasts", package_name) 
 
 # func writing ------------------------------------------------------------
 
-file.edit("pyKS/ks.py")
+file.edit("pyks/ks.py")
 
--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 from version import find_version
 
 setup(
-        name = 'pyKS',
+        name = 'pyks',
         author = 'Jiaxiang Li',
         description = 'Calculate KS for models',
         license = 'MIT',
-        version = find_version('once', '__init__.py'),
+        version = find_version('pyks', '__init__.py'),
         packages = find_packages()
      )
