4c0d264c79727f42ccdc8f6e9aa5f75f9efbe248
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2019-07-23 03:31:49 +0800

    add email

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from version import find_version
 setup(
         name = 'pyks',
         author = 'Jiaxiang Li',
+        author_email = 'alex.lijiaxiang@foxmail.com',
         description = 'Calculate KS for models',
         license = 'MIT',
         version = find_version('pyks', '__init__.py'),
